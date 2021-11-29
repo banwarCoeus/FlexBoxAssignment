ffd60c0f21e0b76447ea030dcff35a0f2e05556b
coeus <coeus@coeuss-mbp.home>
2021-11-29 15:44:21 +0500

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, Text, View, TextInput } from "react-native";
+
+import Screen from "./app/components/Screen";
+import colors from "./app/config/colors";
 
 export default function App() {
+  const [textValue, setTextValue] = useState("");
+  const [flexGrowValue, setFlexGrowValue] = useState(0);
+  const [flexShrinkValue, setFlexShrinkValue] = useState(0);
+  const [textArray, setTextArray] = useState([]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Screen>
+      <TextInput
+        value={textValue}
+        onChangeText={(text) => setTextValue(text)}
+        style={{
+          height: 40,
+          width: "80%",
+          alignSelf: "center",
+          backgroundColor: colors.secondary,
+          padding: 5,
+          borderRadius: 10,
+        }}
+        placeholder="Add text here"
+        placeholderTextColor="white"
+      />
+      <TextInput
+        value={flexGrowValue}
+        onChangeText={(text) => setFlexGrowValue(text)}
+        style={{
+          height: 40,
+          width: "80%",
+          alignSelf: "center",
+          backgroundColor: colors.secondary,
+          padding: 5,
+          borderRadius: 10,
+          marginVertical: 5,
+        }}
+        keyboardType="numeric"
+        placeholder="Add Flex Grow here"
+        placeholderTextColor="white"
+      />
+      <TextInput
+        value={flexShrinkValue}
+        onChangeText={(text) => setFlexShrinkValue(text)}
+        style={{
+          height: 40,
+          width: "80%",
+          alignSelf: "center",
+          backgroundColor: colors.secondary,
+          padding: 5,
+          borderRadius: 10,
+        }}
+        keyboardType="numeric"
+        placeholder="Add Flex Shrink here"
+        placeholderTextColor="white"
+      />
+      <Button
+        title="Add Image"
+        style={{ backgroundColor: colors.primary }}
+        onPress={() =>
+          setTextArray([
+            ...textArray,
+            {
+              body: textValue,
+              flex_grow: flexGrowValue,
+              flex_shrink: flexShrinkValue,
+            },
+          ])
+        }
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          width: "70%",
+          height: 150,
+          borderWidth: 2,
+          borderColor: "red",
+          alignSelf: "center",
+          // flexWrap: "wrap",
+        }}
+      >
+        {textArray.map((value) => (
+          <Text
+            style={{
+              flexGrow: value.flex_grow,
+              flexShrink: value.flex_shrink,
+              backgroundColor: "yellow",
+              marginHorizontal: 2,
+            }}
+          >
+            {value.body}
+          </Text>
+        ))}
+      </View>
+    </Screen>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
